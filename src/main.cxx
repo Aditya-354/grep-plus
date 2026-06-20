5ac61b699924a9fe9a4ff39d18885fd71227d092
Aditya <adigithub354@gmail.com>
2026-06-20 18:23:50 +0530

    reimplemented CLI parser

--- src/main.cxx
+++ src/main.cxx
@@ -1,116 +1,77 @@
-// grep command in C++.
-
-/* Expected working:
- * ./gp -i -o 'pattern' filename
- * ./gp -i 'pattern' filename
- * ./gp -o 'pattern filename
-*/
-
 #include <iostream>
-#include <string>
-#include <string_view>
 #include <print>
-#include <filesystem>
 #include <fstream>
-#include <optional>
+#include <filesystem>
 #include <span>
-
-namespace fs = std::filesystem;
+#include <cstdlib>
 
 constexpr std::string_view case_insensitive_flag { "-i" };
 constexpr std::string_view print_only_pattern_flag { "-o" };
-constexpr std::string_view print_Help { "--help" };
+constexpr std::string_view help_flag { "--help" };
 
 struct Options {
-    std::optional<fs::path> file_object {};
+    std::filesystem::path file_object {};
     std::string pattern {};
-    bool case_insensitive { false };
     bool print_only_pattern { false };
+    bool case_insensitive { false };
+    bool read_stdin { false };
 };
 
-int print_help(int exit_code) {
-    std::println("Usage: grep [OPTIONS]... pattern [FILE]...");
-    std::println("Options:");
-    std::println("-i: Enable case-insensitive search.");
-    std::println("-o: Print only the matching patterns.");
-    exit(exit_code);
+void print_usage(int exit_code) {
+    std::cerr << "Usage: grep [OPTIONS]... PATTERN [FILES]...\n";
+    std::exit(exit_code);
 }
 
-Options parse_print_options(std::span<char*> arguments) {
-    Options options {};
-    size_t i { 1 };
-
-    while (i < arguments.size()) {
-        std::string argument { arguments[i++] };
-
-        if (argument == case_insensitive_flag) {
-            if (i == arguments.size()) {
-                print_help(1);
-                continue;
-            } else options.case_insensitive = true;
-        } else if (argument == print_only_pattern_flag) {
-            if (i == arguments.size()) {
-                print_help(1);
-                continue;
-            } else options.print_only_pattern = true;
-        } else if (argument == print_Help) {
-            print_help(0);
-            continue;
-        } else {
-            if (argument.starts_with('-')) {
-                std::cerr << "Invalid option. Run 'gp --help' for more information.\n";
-                continue;
-            } else options.pattern = argument;
-        }
-
-        if (i == arguments.size()) {
-            fs::path user_path_to_file { argument };
-            if (fs::exists(user_path_to_file)) {
-                if (!fs::is_regular_file(user_path_to_file)) {
-                    std::cerr << "Error: Not a file.\n";
-                    continue;
-                } else {
-                    std::cout << "File exists!\n";
-                    options.file_object = user_path_to_file;
-                }
-            } else {
-                std::cerr << arguments.size() << "\n";
-                std::cerr << "Error: " << argument << " does not exist!\n";
-                continue;
-            }
-        }
-    }
-
-    return options;
+void print_help() {
+    std::cout << "Usage: grep [OPTIONS]... PATTERN [FILES]...\n";
+    std::exit(EXIT_SUCCESS);
 }
 
-class Grep {
-    private:
-        Options m_options {};
-    public:
-        Grep(Options& print_options)
-            : m_options(print_options)
-        {
-            std::cout << "Grep class constructed successfully!\n";
-            std::cout << "printing options struct: \n";
-            std::cout << m_options.case_insensitive << "\n";
-            std::cout << m_options.print_only_pattern << "\n";
-            if (m_options.file_object.has_value()) {
-                std::cout << m_options.file_object->string() << std::endl;
-            }
-        }
+Options parse_options(std::span<char*> arguments) {
+   Options options {};
 
-};
+   size_t i { 1 };
+   while (i < arguments.size()) {
+       std::string argument { arguments[i] };
+       size_t next_index { i + 1 };
 
-int main([[maybe_unused]]int argc, [[maybe_unused]]char** argv) {
-    if (argc < 2) {
-        std::cerr << "usage: gp [OPTION]... PATTERN [FILE]...\n"
-            "Try 'grep --help' for more information.\n";
-        return 1;
-    }
+       if (argument == case_insensitive_flag) {
+           if (next_index == arguments.size()) {
+               print_usage(1);
+               continue;
+           } else options.case_insensitive = true;
+       } else if (argument == print_only_pattern_flag) {
+           if (next_index == arguments.size()) {
+               print_usage(1);
+               continue;
+           } else options.print_only_pattern = true;
+       } else if (options.pattern.size() == 0){
+           options.pattern = argument;
+           if (next_index == arguments.size()) options.read_stdin = true;
+       } else if (std::filesystem::exists(std::filesystem::path(argument)))
+           options.file_object = argument;
+       else { std::cerr << "Error: File '" << argument << "' does not exists.\n"; continue; }
 
-    Options print_options { parse_print_options(std::span<char*>(argv, argc)) };
+       i++;
+   }
 
-    Grep grep { print_options };
+   return options;
+}
+
+void print_all_options(const Options& options) {
+    std::println("print_only_pattern: {}", options.print_only_pattern);
+    std::println("case_insensitive: {}", options.case_insensitive);
+    std::println("pattern: {}", options.pattern);
+    std::println("read_stdin: {}", options.read_stdin);
+    std::println("file_object: {}", options.file_object.string());
+}
+
+void print_grep_output(const Options& options) {
+
+}
+
+int main(int argc, char** argv) {
+    Options options { parse_options(std::span<char*>(argv, argc)) };
+    print_all_options(options);
     return 0;
 }
