16d64b3984d139b71ea107f584e28fae5c0caf65
Aditya <adigithub354@gmail.com>
2026-06-21 12:56:11 +0530

    implemented the working of the '-i' flag

--- GrepHeaders/globals.hpp
+++ GrepHeaders/globals.hpp
@@ -6,4 +6,5 @@ namespace Grep {
     constexpr std::string_view case_insensitive_flag { "-i" };
     constexpr std::string_view print_only_pattern_flag { "-o" };
     constexpr std::string_view help_flag { "--help" };
+    constexpr std::string_view GREP_RESULT_COLOR { "\x1b[31" };
 }
--- src/main.cxx
+++ src/main.cxx
@@ -11,9 +11,7 @@ void print_all_options(const Grep::Options& options) {
     std::println("file_object: {}", options.file_object.string());
 }
 
-void case_insensitive_output(std::ifstream& file, std::string& pattern) {
-
-}
+constexpr std::string_view GREP_RESULT_COLOR { "\x1b[31" };
 
 class GrepCLI {
     private:
@@ -23,20 +21,64 @@ class GrepCLI {
             : m_options(options)
         {}
 
-        void GrepOutput() {
+        bool ipattern(std::string& line, std::string& pattern) const {
+            bool is_pattern_case_insensitive { false };
+            if (pattern.size() > line.size())
+                return is_pattern_case_insensitive;
+
+            for (size_t i { 0 }; i < line.size() - pattern.size(); ++i) {
+                size_t j { 0 };
+
+                while (j < pattern.size()) {
+                    if (std::tolower(line[i + j]) != std::tolower(pattern[j]))
+                        break;
+
+                    ++j;
+                }
+
+                if (j == pattern.size()) is_pattern_case_insensitive = true;
+            }
+
+            return is_pattern_case_insensitive;
+        }
+
+        std::string opattern(std::string& line, std::string& pattern) const {
+            std::string res {};
+            size_t j { 0 };
+
+            for (size_t i { 0 }; i < line.size() - pattern.size(); ++i) {
+                j = 0;
+
+                while (j < pattern.size()) {
+                    if (std::tolower(line[i + j]) != std::tolower(pattern[j])) {
+                        res = "";
+                        break;
+                    }
+
+                    res += line[i + j];
+                    ++j;
+                }
+            }
+
+            return res;
+        }
+
+        void start_grep() {
             std::ifstream file { m_options.file_object.string() };
             std::string line {};
             size_t i { 1 };
 
             if (file.is_open()) {
                 while (std::getline(file, line)) {
-                    if (line.contains(m_options.pattern)) {
-                        if (m_options.case_insensitive) {
-                            case_insensitive_output(file, m_options.pattern);
-                        } else 
+                    if (m_options.case_insensitive) {
+                        if (ipattern(line, m_options.pattern)) {
+                            if (m_options.print_only_pattern) {
+                                std::println("{}:\t{}", i, opattern(line, m_options.pattern));
+                            }
                             std::println("{}:\t{}", i, line);
+                        }
                     }
-                    i++;
+                    ++i;
                 }
 
                 file.close();
@@ -52,6 +94,6 @@ int main(int argc, char** argv) {
     print_all_options(options);
 
     GrepCLI grep { options };
-    grep.GrepOutput();
+    grep.start_grep();
     return 0;
 }
