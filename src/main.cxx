71e10dc77d9dd73900ba21d4d50f2246018606e7
Aditya <adigithub354@gmail.com>
2026-06-22 11:42:57 +0530

    added colored output for -o

--- src/main.cxx
+++ src/main.cxx
@@ -1,9 +1,11 @@
 #include <parser.hpp>
 #include <fstream>
 #include <iostream>
+#include <vector>
 #include <print>
 
-void print_all_options(const Grep::Options& options) {
+void print_all_options(const Grep::Options& options) 
+{
     std::println("print_only_pattern: {}", options.print_only_pattern);
     std::println("case_insensitive: {}", options.case_insensitive);
     std::println("pattern: {}", options.pattern);
@@ -11,9 +13,8 @@ void print_all_options(const Grep::Options& options) {
     std::println("file_object: {}", options.file_object.string());
 }
 
-constexpr std::string_view GREP_RESULT_COLOR { "\x1b[31" };
-
-class GrepCLI {
+class GrepCLI 
+{
     private:
         Grep::Options m_options {};
     public:
@@ -21,97 +22,151 @@ class GrepCLI {
             : m_options(options)
         {}
 
-        bool ipattern(std::string& line, std::string& pattern) const {
-            bool is_pattern_case_insensitive { false };
-            if (pattern.size() > line.size())
-                return is_pattern_case_insensitive;
+        std::vector<size_t> ipattern(std::string& line, std::string& pattern) const
+        {
+            std::vector<size_t> positions {};
 
-            for (size_t i { 0 }; i < line.size() - pattern.size(); ++i) {
+            if (pattern.size() > line.size())
+            {
+                return {};
+            }
+
+            for (size_t i { 0 }; i < line.size() - pattern.size(); ++i)
+            {
                 size_t j { 0 };
 
-                while (j < pattern.size()) {
+                while (j < pattern.size())
+                {
                     if (std::tolower(line[i + j]) != std::tolower(pattern[j]))
+                    {
                         break;
+                    }
 
                     ++j;
                 }
 
-                if (j == pattern.size()) is_pattern_case_insensitive = true;
+                if (j == pattern.size())
+                {
+                    positions.push_back(i);
+                }
             }
 
-            return is_pattern_case_insensitive;
+            return positions;
         }
 
-        std::string opattern(std::string& line, std::string& pattern) const {
+        std::string opattern(std::string& line, std::string& pattern) const
+        {
             std::string res {};
             size_t j { 0 };
 
             if (line.size() < pattern.size()) return "";
 
-            for (size_t i { 0 }; i < line.size() - pattern.size(); ++i) {
+            for (size_t i { 0 }; i < line.size() - pattern.size(); ++i)
+            {
                 j = 0;
 
-                if (m_options.case_insensitive) {
-                    while (j < pattern.size()) {
-                        if (std::tolower(line[i + j]) != std::tolower(pattern[j])) {
+                if (m_options.case_insensitive)
+                {
+                    while (j < pattern.size())
+                    {
+                        if (std::tolower(line[i + j]) != std::tolower(pattern[j]))
+                        {
                             res = "";
                             break;
                         }
 
                         res += line[i + j];
                         ++j;
-                        if (res.size() == pattern.size()) return res;
+                        if (res.size() == pattern.size())
+                        {
+                            return res;
+                        }
                     }
-                } else {
-                    while (j < pattern.size()) {
-                        if (line[i + j] != pattern[j]) {
+                }
+                else
+                {
+                    while (j < pattern.size())
+                    {
+                        if (line[i + j] != pattern[j])
+                        {
                             res = "";
                             break;
                         }
 
                         res += line[i + j];
                         ++j;
-                        if (res.size() == pattern.size()) return res;
+                        if (res.size() == pattern.size())
+                        {
+                            return res;
+                        }
                     }
                 }
             }
             return res;
         }
 
-        void start_grep() {
+        void start_grep()
+        {
             std::ifstream file { m_options.file_object.string() };
             std::string line {};
             size_t i { 1 };
 
-            if (file.is_open()) {
-                while (std::getline(file, line)) {
-                    if (m_options.case_insensitive) {
-                        if (ipattern(line, m_options.pattern)) {
-                            if (m_options.print_only_pattern) {
-                                std::println("{}:{}", i, opattern(line, m_options.pattern));
-                            } else std::println("{}:{}", i, line);
+            if (file.is_open())
+            {
+                while (std::getline(file, line))
+                {
+                    if (m_options.case_insensitive)
+                    {
+                        std::vector<size_t> pattern_positions { ipattern(line, m_options.pattern) };
+
+                        if (!pattern_positions.empty())
+                        {
+                            if (m_options.print_only_pattern)
+                            {
+                                std::println("\x1b[32m{}\x1b[0m:\x1b[31m{}\x1b[0m", i, opattern(line, m_options.pattern));
+                            }
+                            else
+                            {
+                                std::println("\x1b[32m{}\x1b[0m:\x1b[31m{}\x1b[0m", i, line);
+                            }
                         }
-                    } else if (m_options.print_only_pattern) {
-                        if (!line.contains(m_options.pattern)) { ++i; continue; }
-                        std::println("{}:{}", i, opattern(line, m_options.pattern));
-                    } else {
-                        if (!line.contains(m_options.pattern)) {
+                    }
+                    else if (m_options.print_only_pattern)
+                    {
+                        if (!line.contains(m_options.pattern))
+                        { 
+                            ++i; 
+                            continue;
+                        }
+                        std::println("\x1b[32m{}\x1b[0m:\x1b[31m{}\x1b[0m", i, opattern(line, m_options.pattern));
+                    }
+                    else
+                    {
+                        if (!line.contains(m_options.pattern))
+                        {
                             ++i;
                             continue;
-                        } else std::println("{}:{}", i, line);
+                        }
+                        else
+                        {
+                            std::println("\x1b[32m{}\x1b[0m:\x1b[31m{}\x1b[0m", i, line);
+                        }
                     }
                     ++i;
                 }
 
                 file.close();
-            } else {
+            }
+            else
+            {
                 std::cerr << "Error: Unable to open file " << m_options.file_object << "\n";
             }
         }
 };
 
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
     Grep::Options options { Grep::parse_options(std::span<char*>(argv, argc)) };
     print_all_options(options);
 
